9b4a7ea3eab4275efb30d276a572f5c9b4a79ac4
takuya <55338+takuya@users.noreply.github.com>
2023-09-22 17:47:32 +0900

    ajust for /bin/sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ sed -e "s/^relayhost =.*$//g" -i /etc/postfix/main.cf
 echo "${POSTFIX_RELAY_HOST} ${POSTFIX_RELAY_USER}:${POSTFIX_RELAY_PASS}" > /etc/postfix/sasl_passwd
 echo "relayhost = ${POSTFIX_RELAY_HOST}" >> /etc/postfix/main.cf
 
-if [[ ! -z $POSTFIX_RELAY_MYNET ]]; then
+if [ ! -z $POSTFIX_RELAY_MYNET ]; then
   sed  -e "s|^mynetworks.*|mynetworks = $POSTFIX_RELAY_MYNET|" -i /etc/postfix/main.cf
 fi
 ##
